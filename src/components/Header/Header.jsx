230925e67dfdfcc937e579b4cb1c3bcff83c1fb9
Rodriques <rodriques.perry@gmail.com>
2023-05-29 18:11:14 -0500

    Add: HomeScreen.jsx and Product.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
-  return (
-    <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
-        <Container>
-          <Navbar.Brand href='/'>Proshop</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaShoppingCart />
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+				<Container>
+					<Navbar.Brand href='/'>
+						<img src={logo} alt='Shop Logo' />
+						Proshop
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							<Nav.Link href='/cart'>
+								<FaShoppingCart />
+							</Nav.Link>
+							<Nav.Link href='/login'>
+								<FaUser />
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;
