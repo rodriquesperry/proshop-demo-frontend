95016f56bc16a3e84c9111b58b6652608bf0f525
Rodriques <rodriques.perry@gmail.com>
2023-05-29 18:44:24 -0500

    Update: Routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../../assets/images/logo.png';
@@ -8,19 +9,25 @@ const Header = () => {
 		<header>
 			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
 				<Container>
-					<Navbar.Brand href='/'>
-						<img src={logo} alt='Shop Logo' />
-						Proshop
-					</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand>
+							<img src={logo} alt='Shop Logo' />
+							Proshop
+						</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
-							<Nav.Link href='/cart'>
-								<FaShoppingCart />
-							</Nav.Link>
-							<Nav.Link href='/login'>
-								<FaUser />
-							</Nav.Link>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									<FaShoppingCart />
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<FaUser />
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/Home/HomeScreen';
+
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index={true} path='/' element={<HomeScreen />} />
+		</Route>
+	)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
